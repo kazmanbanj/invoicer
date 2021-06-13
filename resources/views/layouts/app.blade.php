d5f072e5dac115978e42fe5a569adf1301071ecd
Banjoko Kazeem <banjoko10@gmail.com>
2021-06-13 22:55:30 +0100

    renamed app name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'KB-Invoice') }}</title>
+    <title>{{ config('app.name', 'KB-Invoicer') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
