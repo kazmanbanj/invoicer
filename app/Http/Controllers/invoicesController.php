d1190d643f53b927db0b747f2ed4e33526d145e6
Banjoko Kazeem <banjoko10@gmail.com>
2021-06-13 14:52:38 +0100

    Saving to db eloquent model migrations

--- app/Http/Controllers/invoicesController.php
+++ app/Http/Controllers/invoicesController.php
@@ -2,10 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Invoice;
+use App\Models\Customer;
+use App\Models\InvoicesItem;
+use App\Models\CustomersField;
 use Illuminate\Http\Request;
 
 class invoicesController extends Controller
 {
+    public function index()
+    {
+        return 'index';
+    }
+
     public function create()
     {
         return view('invoices.create');
@@ -13,6 +22,30 @@ class invoicesController extends Controller
 
     public function store(Request $request)
     {
+        $customer = Customer::create($request->customer);
+        $invoice = Invoice::create($request->invoice + ['customer_id' => $customer->id]);
+
+        for ($i=0; $i < count($request->product); $i++) {
+            if (isset($request->qty[$i]) && isset($request->price[$i])) {
+                InvoicesItem::create([
+                    'invoice_id' => $invoice->id,
+                    'name' => $request->product[$i],
+                    'quantity' => $request->qty[$i],
+                    'price' => $request->price[$i]
+                ]);
+            }
+        }
+
+        for ($i=0; $i < count($request->customer_fields); $i++) {
+            if (isset($request->customer_fields[$i]['field_key']) && isset($request->customer_fields[$i]['field_value'])) {
+                CustomersField::create([
+                    'customer_id' => $customer->id,
+                    'field_key' => $request->customer_fields[$i]['field_key'],
+                    'field_value' => $request->customer_fields[$i]['field_value']
+                ]);
+            }
+        }
+
         return 'hi';
     }
 }
--- resources/views/invoices/create.blade.php
+++ resources/views/invoices/create.blade.php
@@ -8,7 +8,7 @@
                 <div class="card-header">Create Invoice</div>
 
                 <form action="{{ route('invoices.store') }}" method="post">
-                @csrf
+                    @csrf
                     <div class="card-body">
                         <div class="container">
                             <div class="row clearfix">
@@ -42,14 +42,13 @@
                                                         <th class="text-center" width="50%">Field</th>
                                                         <th class="text-center">Value</th>
                                                     </tr>
-                                                    @for ($i = 0; $i <= 2; $i++)
-                                                        <tr>
-                                                            <td class="text-center">
-                                                                <input type="text" name="customer_fields[{{ $i }}][field_key]" id="" class="form-control">
-                                                            </td>
-                                                            <td class="text-center">
-                                                                <input type="text" name="customer_fields[{{ $i }}][field_value]" id="" class="form-control">
-                                                            </td>
+                                                    @for ($i = 0; $i <= 2; $i++) <tr>
+                                                        <td class="text-center">
+                                                            <input type="text" name="customer_fields[{{ $i }}][field_key]" id="" class="form-control">
+                                                        </td>
+                                                        <td class="text-center">
+                                                            <input type="text" name="customer_fields[{{ $i }}][field_value]" id="" class="form-control">
+                                                        </td>
                                                         </tr>
                                                     @endfor
                                                 </tbody>
@@ -81,14 +80,14 @@
                                                 </tr>
                                             </thead>
                                             <tbody>
-                                            <tr id='addr0'>
-                                                <td>1</td>
-                                                <td><input type="text" name='product[]'  placeholder='Enter Product Name' class="form-control"/></td>
-                                                <td><input type="number" name='qty[]' placeholder='Enter Qty' class="form-control qty" step="0" min="0"/></td>
-                                                <td><input type="number" name='price[]' placeholder='Enter Unit Price' class="form-control price" step="0.00" min="0"/></td>
-                                                <td><input type="number" name='total[]' placeholder='0.00' class="form-control total" readonly/></td>
-                                            </tr>
-                                            <tr id='addr1'></tr>
+                                                <tr id='addr0'>
+                                                    <td>1</td>
+                                                    <td><input type="text" name='product[]' placeholder='Enter Product Name' class="form-control" /></td>
+                                                    <td><input type="number" name='qty[]' placeholder='Enter Qty' class="form-control qty" step="0" min="0" /></td>
+                                                    <td><input type="number" name='price[]' placeholder='Enter Unit Price' class="form-control price" step="0.00" min="0" /></td>
+                                                    <td><input type="number" name='total[]' placeholder='0.00' class="form-control total" readonly /></td>
+                                                </tr>
+                                                <tr id='addr1'></tr>
                                             </tbody>
                                         </table>
                                     </div>
@@ -96,34 +95,36 @@
                             </div>
                             <div class="row clearfix">
                                 <div class="col-md-12">
-                                    <button id="add_row" class="btn btn-primary float-left">Add Row</button>
-                                    <button id='delete_row' class="float-right btn btn-danger">Delete Row</button>
+                                    <button type="button" id="add_row" class="btn btn-primary float-left">Add Row</button>
+                                    <button type="button" id='delete_row' class="float-right btn btn-danger">Delete Row</button>
                                 </div>
                             </div>
                             <div class="row clearfix" style="margin-top:20px">
                                 <div class="col-md-12">
-                                    <div class="float-right col-md-5">
+                                    <div class="float-right col-md-7">
                                         <table class="table table-bordered table-hover" id="tab_logic_total">
                                             <tbody>
-                                            <tr>
-                                                <th class="text-center" width="50%">Sub Total</th>
-                                                <td class="text-center"><input type="number" name='sub_total' placeholder='0.00' class="form-control" id="sub_total" readonly/></td>
-                                            </tr>
-                                            <tr>
-                                                <th class="text-center">Tax</th>
-                                                <td class="text-center"><div class="input-group mb-2 mb-sm-0">
-                                                    <input type="number" class="form-control" id="tax" placeholder="0">
-                                                    <div class="input-group-addon">%</div>
-                                                </div></td>
-                                            </tr>
-                                            <tr>
-                                                <th class="text-center">Tax Amount</th>
-                                                <td class="text-center"><input type="number" name='tax_amount' id="tax_amount" placeholder='0.00' class="form-control" readonly/></td>
-                                            </tr>
-                                            <tr>
-                                                <th class="text-center">Grand Total</th>
-                                                <td class="text-center"><input type="number" name='total_amount' id="total_amount" placeholder='0.00' class="form-control" readonly/></td>
-                                            </tr>
+                                                <tr>
+                                                    <th class="text-center" width="50%">Sub Total</th>
+                                                    <td class="text-center"><input type="number" name='sub_total' placeholder='0.00' class="form-control" id="sub_total" readonly /></td>
+                                                </tr>
+                                                <tr>
+                                                    <th class="text-center">Tax</th>
+                                                    <td class="text-center">
+                                                        <div class="input-group mb-2 mb-sm-0">
+                                                            <input type="number" class="form-control" id="tax" placeholder="0" name="invoice[tax_percent]">
+                                                            <div class="input-group-addon">%</div>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <th class="text-center">Tax Amount</th>
+                                                    <td class="text-center"><input type="number" name='tax_amount' id="tax_amount" placeholder='0.00' class="form-control" readonly /></td>
+                                                </tr>
+                                                <tr>
+                                                    <th class="text-center">Grand Total</th>
+                                                    <td class="text-center"><input type="number" name='total_amount' id="total_amount" placeholder='0.00' class="form-control" readonly /></td>
+                                                </tr>
                                             </tbody>
                                         </table>
                                     </div>
@@ -141,56 +142,54 @@
 
 @section('javascript')
 <script>
-$(document).ready(function(){
-    var i=1;
-    $("#add_row").click(function(){b=i-1;
-      	$('#addr'+i).html($('#addr'+b).html()).find('td:first-child').html(i+1);
-      	$('#tab_logic').append('<tr id="addr'+(i+1)+'"></tr>');
-      	i++;
-  	});
-    $("#delete_row").click(function(){
-    	if(i>1){
-		$("#addr"+(i-1)).html('');
-		i--;
-		}
-		calc();
-	});
+$(document).ready(function() {
+    var i = 1;
+    $("#add_row").click(function() {
+        b = i - 1;
+        $('#addr' + i).html($('#addr' + b).html()).find('td:first-child').html(i + 1);
+        $('#tab_logic').append('<tr id="addr' + (i + 1) + '"></tr>');
+        i++;
+    });
+    $("#delete_row").click(function() {
+        if (i > 1) {
+            $("#addr" + (i - 1)).html('');
+            i--;
+        }
+        calc();
+    });
 
-	$('#tab_logic tbody').on('keyup change',function(){
-		calc();
-	});
-	$('#tax').on('keyup change',function(){
-		calc_total();
-	});
+    $('#tab_logic tbody').on('keyup change', function() {
+        calc();
+    });
+    $('#tax').on('keyup change', function() {
+        calc_total();
+    });
 
 
 });
 
-function calc()
-{
-	$('#tab_logic tbody tr').each(function(i, element) {
-		var html = $(this).html();
-		if(html!='')
-		{
-			var qty = $(this).find('.qty').val();
-			var price = $(this).find('.price').val();
-			$(this).find('.total').val(qty*price);
+function calc() {
+    $('#tab_logic tbody tr').each(function(i, element) {
+        var html = $(this).html();
+        if (html != '') {
+            var qty = $(this).find('.qty').val();
+            var price = $(this).find('.price').val();
+            $(this).find('.total').val(qty * price);
 
-			calc_total();
-		}
+            calc_total();
+        }
     });
 }
 
-function calc_total()
-{
-	total=0;
-	$('.total').each(function() {
+function calc_total() {
+    total = 0;
+    $('.total').each(function() {
         total += parseInt($(this).val());
     });
-	$('#sub_total').val(total.toFixed(2));
-	tax_sum=total/100*$('#tax').val();
-	$('#tax_amount').val(tax_sum.toFixed(2));
-	$('#total_amount').val((tax_sum+total).toFixed(2));
+    $('#sub_total').val(total.toFixed(2));
+    tax_sum = total / 100 * $('#tax').val();
+    $('#tax_amount').val(tax_sum.toFixed(2));
+    $('#total_amount').val((tax_sum + total).toFixed(2));
 }
 </script>
 @stop
