169beee510e5fcd8229ee5fcbf5f06a90c0825df
Kazeem Banjoko <banjoko10@gmail.com>
2022-07-09 23:13:32 +0100

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -63,7 +63,7 @@
                                 <a class="nav-link" href="{{ route('products.index') }}">Products</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('customers.index') }}">Customer</a>
+                                <a class="nav-link" href="{{ route('customers.index') }}">Customers</a>
                             </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
